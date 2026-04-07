3d0c36efad4976481acaaf764c43703a2763c240
kanaka maha lakshmi cheduluri <mahalakshmicheduluri@gmail.com>
2026-04-07 18:00:38 +0530

    Delete server directory

--- server/Routes/MeditationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../Middleware/authMiddleware");
-const { addMeditationToday } = require("../Controllers/MeditationController");
-
-router.post("/today", authMiddleware, addMeditationToday);
-
-module.exports = router;
